25c7565eeb00071b6f0f31830182365a6f8eb418
Donald Clements <silveranstavern@yahoo.com>
2018-05-23 09:17:25 -0400

    TaskItem UnitOfWork and Repository building.

---
+++ NotifyWebApi/NotifyWebApi.Repositories/TaskItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
+using NotifyWebApi.Models;
+using NotifyWebApi.Repositories.Interfaces;
+
+namespace NotifyWebApi.Repositories
+{
+    public class TaskItemRepository : Repository<TaskItem>, ITaskRepository
+    {
+        public TaskItemRepository(DbContext context) : base(context)
+        {
+        }
+
+        public NotifyContext RevViewsContext => Context as NotifyContext;
+    }
+}
--- NotifyWebApi/NotifyWebApi.Repositories/UnitOfWork.cs
+++ NotifyWebApi/NotifyWebApi.Repositories/UnitOfWork.cs
@@ -4,15 +4,23 @@ namespace NotifyWebApi.Repositories
 {
     public class UnitOfWork: IUnitOfWork
     {
+        private readonly NotifyContext _context;
+
+        public UnitOfWork() {
+            _context = new NotifyContext();
+            Tasks = new TaskItemRepository(_context);
+        }
+
+
         public void Dispose()
         {
-            throw new System.NotImplementedException();
+             _context.Dispose();
         }
 
         public ITaskRepository Tasks { get; }
         public int Complete()
         {
-            throw new System.NotImplementedException();
+            return _context.SaveChanges();
         }
     }
 }
