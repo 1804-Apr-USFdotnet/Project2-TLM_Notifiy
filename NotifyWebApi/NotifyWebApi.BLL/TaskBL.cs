26ffc4155049d9dc9d5f3e60568f75cff876ef12
Donald Clements <silveranstavern@yahoo.com>
2018-05-24 09:21:43 -0400

    Finished TaskBL by adding Post and formatting code with Resharper.

--- NotifyWebApi/NotifyWebApi.BLL/TaskBL.cs
+++ NotifyWebApi/NotifyWebApi.BLL/TaskBL.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using AutoMapper;
 using NotifyWebApi.BLL.Models;
 using NotifyWebApi.Models;
@@ -11,106 +9,101 @@ using NotifyWebApi.Repositories.Interfaces;
 
 namespace NotifyWebApi.BLL
 {
-    class TaskBL
+    internal class TaskBl
     {
         //ToDo Extract Logic to BLL and switch context out with UnitOfWork.
-        private static readonly MapperConfiguration config = new MapperConfiguration(cfg => {
+        private static readonly MapperConfiguration Config = new MapperConfiguration(cfg =>
+        {
             cfg.CreateMap<TaskItem, TaskItemDto>();
-            cfg.CreateMap<TaskItemDto , TaskItem>();
+            cfg.CreateMap<TaskItemDto, TaskItem>();
             cfg.CreateMap<IEnumerable<TaskItem>, List<TaskItemDto>>();
         });
-
-        readonly IMapper mapper = config.CreateMapper();
 
 
         private readonly IUnitOfWork _uoWork;
 
+        private readonly IMapper _mapper = Config.CreateMapper();
+
 
         /// <summary>
-        /// Default parameterless constructor that sets the private field
-        /// IUnitOfWork to the application specific implementation: UnitOfWork. 
+        ///     Default parameterless constructor that sets the private field
+        ///     IUnitOfWork to the application specific implementation: UnitOfWork.
         /// </summary>
-        public TaskBL()
+        public TaskBl()
         {
             _uoWork = new UnitOfWork();
         }
 
         /// <summary>
-        /// Parameterised version of the constructor that allows IUnitOfWork to be set to
-        /// a mock version.  This is the aspect that makes it testable without affecting the database.
+        ///     Parameterised version of the constructor that allows IUnitOfWork to be set to
+        ///     a mock version.  This is the aspect that makes it testable without affecting the database.
         /// </summary>
         /// <param name="littleWorker"></param>
-
-        public TaskBL(IUnitOfWork littleWorker)
+        public TaskBl(IUnitOfWork littleWorker)
         {
             _uoWork = littleWorker;
         }
 
+
         public IEnumerable<TaskItemDto> GetTaskItems(long userId)
         {
-            var tasks =_uoWork.Tasks.GetAll().Where(o=>o.UserId == userId);
-            IEnumerable<TaskItemDto> dto = mapper.Map<IEnumerable<TaskItem>, IEnumerable<TaskItemDto>>(tasks);
+            var tasks = _uoWork.Tasks.GetAll().Where(o => o.UserId == userId);
+            var dto = _mapper.Map<IEnumerable<TaskItem>, IEnumerable<TaskItemDto>>(tasks);
             return dto;
         }
 
         public TaskItemDto GetTaskItem(long userId, long taskId)
         {
-
-            TaskItem taskItem = _uoWork.Tasks.Get(taskId);
+            var taskItem = _uoWork.Tasks.Get(taskId);
             if (taskItem.UserId == userId)
             {
-                TaskItemDto dto = mapper.Map<TaskItem, TaskItemDto>(taskItem);
+                var dto = _mapper.Map<TaskItem, TaskItemDto>(taskItem);
                 return dto;
             }
+
             return null; //Controller will catch know and redirect to bad request.
         }
 
-        // PUT: api/TaskItems/5
+
         public int PutTaskItem(TaskItemDto taskItemDto)
         {
             try
             {
                 var taskItem = _uoWork.Tasks.Get(taskItemDto.TaskId);
-                taskItem = mapper.Map<TaskItemDto, TaskItem>(taskItemDto, taskItem);
+                taskItem = _mapper.Map(taskItemDto, taskItem);
                 _uoWork.Tasks.Update(taskItem);
                 _uoWork.Complete();
-                return 204;//No Content
+                return 204; //No Content
             }
             catch (Exception e)
             {
                 //todo implement logger
                 return 500;
             }
-            
-            
         }
 
-        //    // POST: api/TaskItems
-        //    [ResponseType(typeof(TaskItem))]
-        //    public IHttpActionResult PostTaskItem(TaskItem taskItem)
-        //    {
-        //        if (!ModelState.IsValid)
-        //        {
-        //            return BadRequest(ModelState);
-        //        }
 
-        //        db.TaskItems.Add(taskItem);
-        //        db.SaveChanges();
-
-        //        return CreatedAtRoute("DefaultApi", new {id = taskItem.TaskId}, taskItem);
-        //    }
-
-        // DELETE: api/TaskItems/5
+        public int PostTaskItem(TaskItemDto taskItemDto)
+        {
+            var taskItem = _mapper.Map<TaskItemDto, TaskItem>(taskItemDto);
+            try
+            {
+                _uoWork.Tasks.Remove(taskItem);
+                _uoWork.Complete();
+                return 204;
+            }
+            catch (Exception e)
+            {
+                return 500;
+            }
+        }
 
         public int DeleteTaskItem(long userId, long taskId)
         {
-            TaskItem taskItem = _uoWork.Tasks.Get(taskId);
-            if (taskItem==null)
-            {
-                return 404;  //Not Found
-            }
+            var taskItem = _uoWork.Tasks.Get(taskId);
+            if (taskItem == null) return 404; //Not Found
+
             if (taskItem.UserId == userId)
-            {
                 try
                 {
                     _uoWork.Tasks.Remove(taskItem);
@@ -121,34 +114,8 @@ namespace NotifyWebApi.BLL
                 {
                     return 500;
                 }
-                
-            }
+
             return 401; //Unauthorized
-          
-            //if (taskItem == null)
-            //{
-            //    return NotFound();
-            //}
-
-            //db.TaskItems.Remove(taskItem);
-            //db.SaveChanges();
-
-            //return Ok(taskItem);
         }
-
-        //    protected override void Dispose(bool disposing)
-        //    {
-        //        if (disposing)
-        //        {
-        //            db.Dispose();
-        //        }
-
-        //        base.Dispose(disposing);
-        //    }
-
-        //    private bool TaskItemExists(long id)
-        //    {
-        //        return db.TaskItems.Count(e => e.TaskId == id) > 0;
-        //    }
     }
 }
