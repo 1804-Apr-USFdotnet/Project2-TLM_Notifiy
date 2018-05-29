c139a5c826f03453e938f1bacb5c33db7a33964b
Donald Clements <silveranstavern@yahoo.com>
2018-05-28 20:33:49 -0400

    Uri: Removed HTTP to see if that resolved anything

--- NotifyMvcClient/Notify/Controllers/AServiceController.cs
+++ NotifyMvcClient/Notify/Controllers/AServiceController.cs
@@ -9,7 +9,7 @@ namespace Notify.Controllers
     public abstract class AServiceController : Controller
     {
         protected static readonly HttpClient HttpClient = new HttpClient(new HttpClientHandler() { UseCookies = false });
-        protected static readonly Uri serviceUri = new Uri("http://172.31.39.15/NotifyWebApi/");
+        protected static readonly Uri serviceUri = new Uri("172.31.39.15/NotifyWebApi/");
         private static readonly string tokenName = "accessToken";
 
         protected HttpRequestMessage CreateRequestToService(HttpMethod method, string uri)
