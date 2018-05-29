34f01e29cdfcb2530894eb41a15ac618390d182a
Donald Clements <silveranstavern@yahoo.com>
2018-05-28 21:06:04 -0400

    Changed url's that were hardcoded into ones that are grabbed from the abstract service controler.

--- NotifyMvcClient/Notify/Controllers/AServiceController.cs
+++ NotifyMvcClient/Notify/Controllers/AServiceController.cs
@@ -9,7 +9,7 @@ namespace Notify.Controllers
     public abstract class AServiceController : Controller
     {
         protected static readonly HttpClient HttpClient = new HttpClient(new HttpClientHandler() { UseCookies = false });
-        protected static readonly Uri serviceUri = new Uri("http://18.218.238.212:80/NotifyWebApi/");
+        protected static readonly Uri serviceUri = new Uri("http://18.218.238.212/NotifyWebApi/");
         private static readonly string tokenName = "accessToken";
 
         protected HttpRequestMessage CreateRequestToService(HttpMethod method, string uri)
